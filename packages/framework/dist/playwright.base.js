cd4b229593f15c35f5274a316b34b52129f3b4ad
Santiago Gasca <sgasca@playtechla.com>
2025-08-25 11:14:46 -0500

    implement login tests for seller web

--- packages/framework/dist/playwright.base.js
+++ packages/framework/dist/playwright.base.js
@@ -8,14 +8,8 @@ const baseConfig = (0, test_1.defineConfig)({
         trace: 'on-first-retry',
         screenshot: 'only-on-failure',
         //video: 'retain-on-failure',
-        baseURL: process.env.BASE_URL,
         headless: false,
         viewport: { width: 1280, height: 720 }
-    },
-    reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
-    projects: [
-        { name: 'chromium', use: { ...test_1.devices['Desktop Chrome'] } },
-        { name: 'firefox', use: { ...test_1.devices['Desktop Firefox'] } }
-    ]
+    }
 });
 exports.default = baseConfig;
--- packages/framework/playwright.base.ts
+++ packages/framework/playwright.base.ts
@@ -7,15 +7,9 @@ const baseConfig = defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     //video: 'retain-on-failure',
-    baseURL: process.env.BASE_URL,
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720 }
-  },
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } }
-  ]
+  }
 });
 
 export default baseConfig;
