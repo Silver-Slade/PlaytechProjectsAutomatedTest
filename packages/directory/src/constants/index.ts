49273292f5d6807e03e0713edf2790d9e4f49909
Santiago Gasca <sgasca@playtechla.com>
2025-08-27 16:48:55 -0500

    add tests for Directory project

--- packages/directory/src/constants/index.ts
+++ packages/directory/src/constants/index.ts
@@ -1,10 +1,25 @@
 export const Titles = {
-  adminDashboard: '¿Qué deseas hacer hoy?'
+  dashboardTitle: '¿Qué deseas hacer hoy?',
+  successfulLoginMessage: '¡Inicio de sesión exitoso!',
+  welcomeMessage: 'Hola '
+} as const;
+
+export const KeyWords = {
+  testWord: 'Test'
+} as const;
+
+export const Strings = {
+  emptyString: '',
 } as const;
 
 export const NonExistentUser = {
-    username: 'invalidUser',
+    username: 'invalidUser@test.com',
     password: 'invalidPassword'
+}
+
+export const PageConstantMessages = {
+  badEmailFormat: 'Ingrese un correo "xxxx@xxxxx.xxx" válido',
+  requiredField: 'Campo obligatorio'
 }
 
 export type Role = 'admin' | 'leader' | 'dataEntry';
@@ -12,14 +27,17 @@ export type Role = 'admin' | 'leader' | 'dataEntry';
 export const Users = {
   admin: {
     username: process.env.USERNAME_ADMIN ?? '',
-    password: process.env.PASSWORD_ADMIN ?? ''
+    password: process.env.PASSWORD_ADMIN ?? '',
+    name: 'UsuarioAdmin'
   },
   leader: {
     username: process.env.USERNAME_LEADER ?? '',
-    password: process.env.PASSWORD_LEADER ?? ''
+    password: process.env.PASSWORD_LEADER ?? '',
+    name: 'nombreLíder'
   },
   dataEntry: {
     username: process.env.USERNAME_DATAENTRY ?? '',
-    password: process.env.PASSWORD_DATAENTRY ?? ''
+    password: process.env.PASSWORD_DATAENTRY ?? '',
+    name: 'nombreDigitador'
   }
-} satisfies Record<Role, { username: string; password: string }>;
+} satisfies Record<Role, { username: string; password: string; name: string }>;
--- packages/directory/src/tests/admin/login/adminLogin.spec.ts
+++ packages/directory/src/tests/admin/login/adminLogin.spec.ts
@@ -1,11 +1,58 @@
 import { test, expect } from '@playwright/test';
 import LoginPage from '../../../pages/login/LoginPage';
-import { NonExistentUser, Titles, Users } from '../../../constants/index';
+import { NonExistentUser, Users, KeyWords, Strings, PageConstantMessages } from '../../../constants/index';
 
 test('DIR-000001 Login with valid credentials as web administrator', async ({ page }) => {
     const adminLoginPage = new LoginPage(page);
     await adminLoginPage.navigateToAdminLogin();
     await adminLoginPage.loginAsAdmin();
-    await adminLoginPage.isLoggedIn();
-    //await expect(adminLoginPage.dashboardTitle).toHaveText(Titles.adminDashboard);
+    await adminLoginPage.isLoggedIn(page);
+});
+
+test('DIR-000002 Login with non existent user', async ({ page }) => {
+    const adminLoginPage = new LoginPage(page);
+    await adminLoginPage.navigateToAdminLogin();
+    await adminLoginPage.usernameInput.fill(NonExistentUser.username || "");
+    await adminLoginPage.passwordInput.fill(NonExistentUser.password || "");
+    await adminLoginPage.submitButton.click();
+    await expect(adminLoginPage.alertPopUp).toBeVisible();
+    await expect(adminLoginPage.wrongPasswordMessage).toHaveText('Usuario o contraseña incorrectos')
+});
+
+test('DIR-000003 Login with empty credentials', async ({ page }) => {
+    const adminLoginPage = new LoginPage(page);
+    const usernameRedAlert = adminLoginPage.fieldRedAlert.nth(0);
+    const passwordRedAlert = adminLoginPage.fieldRedAlert.nth(1);
+
+    await adminLoginPage.navigateToAdminLogin();
+    await adminLoginPage.submitButton.click();
+    await expect(adminLoginPage.alertPopUp).toBeHidden();
+    await expect(adminLoginPage.fieldRedAlert).toHaveCount(2);
+    await expect(usernameRedAlert).toHaveText(PageConstantMessages.requiredField);
+    await expect(passwordRedAlert).toHaveText(PageConstantMessages.requiredField);
+});
+
+test('DIR-000004 Login with invalid email format in username field', async ({ page }) => {
+    const adminLoginPage = new LoginPage(page);
+    const usernameRedAlert = adminLoginPage.fieldRedAlert.nth(0);
+    const passwordRedAlert = adminLoginPage.fieldRedAlert.nth(1);
+
+    await adminLoginPage.navigateToAdminLogin();
+    await adminLoginPage.usernameInput.fill(KeyWords.testWord || "");
+    await adminLoginPage.passwordInput.fill(NonExistentUser.password || "");
+    await adminLoginPage.submitButton.click();
+    await expect(adminLoginPage.alertPopUp).toBeHidden();
+    await expect(adminLoginPage.fieldRedAlert).toHaveCount(2);
+    await expect(usernameRedAlert).toHaveText(PageConstantMessages.badEmailFormat);
+    await expect(passwordRedAlert).toHaveText(Strings.emptyString);
+});
+
+test('DIR-000005 Login with an existent user but wrong password', async ({ page }) => {
+    const adminLoginPage = new LoginPage(page);
+    await adminLoginPage.navigateToAdminLogin();
+    await adminLoginPage.usernameInput.fill(Users.admin.username || "");
+    await adminLoginPage.passwordInput.fill(NonExistentUser.password || "");
+    await adminLoginPage.submitButton.click();
+    await expect(adminLoginPage.alertPopUp).toBeVisible();
+    await expect(adminLoginPage.wrongPasswordMessage).toHaveText('Usuario o contraseña incorrectos')
 });
