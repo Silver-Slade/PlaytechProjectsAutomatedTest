810754f2e46a1fb8142604fccbc9cedeb87e33c6
Santiago Gasca <sgasca@playtechla.com>
2025-08-29 16:46:04 -0500

    increment global timeouts and turn on the headless mode

--- packages/framework/playwright.base.ts
+++ packages/framework/playwright.base.ts
@@ -2,12 +2,12 @@ import { defineConfig, devices } from '@playwright/test';
 
 const baseConfig = defineConfig({
   timeout: 30_000,
-  expect: { timeout: 5_000 },
+  expect: { timeout: 10_000 },
   use: {
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     //video: 'retain-on-failure',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 }
   }
 });
